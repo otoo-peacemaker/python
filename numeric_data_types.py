7f1c04a3e463b17d89c83dbf257a7f3aca1a464b
otoo-peacemaker <nanakwesiotoo@gmail.com>
2024-05-06 16:35:34 +0000

    numeric data types

---
+++ numeric_data_types.py
@@ -0,0 +1,14 @@
+# Integer
+x = 10
+print(x)  # Output: 10
+
+# Floating-point number
+y = 3.14
+print(y)  # Output: 3.14
+
+# Complex number
+x = 2 + 3j
+y = 5j + 3
+z = x + y
+
+print(z)  # Output: (5+8j)
