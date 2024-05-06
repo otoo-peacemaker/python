924c34e7ac4db9ba76ea2a7c479f81fb7e61f0e7
otoo-peacemaker <nanakwesiotoo@gmail.com>
2024-05-06 16:41:47 +0000

    string data types

---
+++ string_data_types.py
@@ -0,0 +1,24 @@
+# Example of a single-line string
+str1 = "Hello, world!"
+print(str1)  # Output: Hello, world!
+
+
+# Example of a multi-line string
+str2 = """This is a multi-line
+string in Python."""
+print(str2)
+# Output:
+# This is a multi-line
+# string in Python.
+
+
+# Example of string concatenation
+str3 = "Hello" + " " + "world!"
+print(str3)  # Output: Hello world!
+
+
+# Example of string interpolation
+name = "John"
+age = 30
+str4 = f"My name is {name} and I am {age} years old."
+print(str4)  # Output: My name is John  I am 30 years old.
